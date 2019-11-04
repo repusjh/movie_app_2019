d03c8c99ab20c5927f3ac28964544718bfe431ad
repusjh <repusjh@gmail.com>
2019-11-04 23:21:22 +0900

    class component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  state = {
+    count : 0
+  };
+  add = () => {
+    this.setState({count : this.state.count + 1})
+  };
+  minus = () => {
+    this.setState({count : this.state.count - 1})
+  };
+
+  render(){
+    return (
+      <div>
+        <h1>This is number {this.state.count}</h1>
+        <button onClick={this.add}>add</button>
+        <button onClick={this.minus}>minus</button>
+      </div>
+    )
+  }
 }
 
 export default App;
